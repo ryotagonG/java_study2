baaae2cdccec9c253a62400a9dc3814df8c1077b
ryotagonG <87141681+ryotagonG@users.noreply.github.com>
2021-12-18 16:46:10 +0900

    javaの基礎

---
+++ helloworld.java
@@ -0,0 +1,19 @@
+public class helloworld {
+  public static void main(String[] args) {    //javaプログラムを実行するために必要な形式的なもの
+
+// //文字を出力するプログラム
+//     System.out.println("hello world");
+//     System.out.println("私の名前は〇〇です");
+//     System.out.println("よろしくお願いします");
+
+// //演算を行うプログラム
+//     System.out.println(3*5);
+
+//課題１
+    String name = "tanaka";
+    System.out.println("私の名前は" + name + "です");
+    int score = 4/2;
+    System.out.println("１たす１は" + score + "です");
+    System.out.println("４わる２は" + score + "です");
+  }
+}
